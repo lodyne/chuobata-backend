8b221d76186818edb7fb0a92885adff0db9511d3
lodyne <lodgmtui@gmail.com>
2025-05-07 12:30:16 +0300

    add django models

--- djapps/course/models.py
+++ djapps/course/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+    class Meta:
+        db_table = 'course'
+        verbose_name = 'Course' 
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.name
