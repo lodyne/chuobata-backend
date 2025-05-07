ab6c0be3c4fa525d08a963d8bb7589dc0f68895f
lodyne <lodgmtui@gmail.com>
2025-05-07 12:30:35 +0300

    add django views

--- djapps/course/views.py
+++ djapps/course/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_courses(request):
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/list_courses.html')
+
+def course_detail(request, course_id):
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/course_detail.html', {'course_id': course_id})
+
+def create_course(request):
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/create_course.html')
+
+def update_course(request, course_id): 
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/update_course.html', {'course_id': course_id})
+
+def delete_course(request, course_id):
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/delete_course.html', {'course_id': course_id})
+
+def enroll_in_course(request, course_id):
+    # This is a placeholder view. You can implement your logic here.
+    return render(request, 'course/enroll_in_course.html', {'course_id': course_id})
