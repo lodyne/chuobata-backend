12a2b84fd17f4d65c1dfe5d3f735de4e4f1347dd
lodyne <lodgmtui@gmail.com>
2025-07-25 14:53:10 +0300

    feat(views):create views for registration

--- djapps/users/views.py
+++ djapps/users/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, login
+from django.contrib.auth import get_user_model
+from .forms import RegisterForm
 
+User = get_user_model()
 
 @login_required
 def home(request):
@@ -43,4 +46,29 @@ def custom_logout(request):
     logout(request)
     return redirect('home')
     
-    
+
+
+def register(request):
+    template_name = 'users/register.html'
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password2 = form.cleaned_data['password2']
+            email = form.cleaned_data['email']
+
+            if password != password2:
+                form.add_error('password2', 'Passwords do not match')
+            elif User.objects.filter(username=username).exists():
+                form.add_error('username', 'Username already exists.')
+            elif User.objects.filter(email=email).exists():
+                form.add_error('email', 'Email already exists.')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                login(request, user)
+                return redirect('home')
+    else:
+        form = RegisterForm()
+    return render(request, template_name, {'form': form})
