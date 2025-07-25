8b1b7d45a92e3c79180a987d757cda541a8935c3
lodyne <lodgmtui@gmail.com>
2025-07-25 14:45:03 +0300

    feat(urls):add new paths in the urls

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("djapps.users.urls")),
     path("schema/", include("schema_viewer.urls")),
+    path("accounts/",include("allauth.urls"))
 ]
--- djapps/users/urls.py
+++ djapps/users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from djapps.users.views import custom_login, custom_logout, home
+from djapps.users.views import custom_login, custom_logout, home, register
+
 
 urlpatterns = [
     path("", home,name='home'),
     path("login/", custom_login, name='login'),
     path("logout/", custom_logout, name='logout'),
+    path("register/",register, name="register")
 ]
